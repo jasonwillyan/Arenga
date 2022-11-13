1d37837f5bf2a95ff970ce1a73936a280b5dbff3
jasonwillyan <jasonwillyan@hotmail.com>
2022-11-13 01:28:20 -0300

    create dark mode button

--- js/tooltip-gif.js
+++ js/tooltip-gif.js
@@ -9,8 +9,8 @@ void (function generate_tooltip_gif() {
     const div = document.createElement("div");
     const img = document.createElement("img");
     const tag_i = document.createElement("i");
- 
-    if(position === "bottom") tag_i.classList.add("diamond");
+
+    if (position === "bottom") tag_i.classList.add("diamond");
     else tag_i.classList.add("diamond-top");
 
     div.classList.add(position);
@@ -19,5 +19,42 @@ void (function generate_tooltip_gif() {
     div.appendChild(img);
     div.appendChild(tag_i);
     a.appendChild(div);
+  }
+})();
+
+// eslint-disable-next-line no-unused-vars
+function change_theme() {
+  if (!document.getElementById("dark-mode").checked) {
+    document.documentElement.style.setProperty("--main-color", "#000");
+    document.documentElement.style.setProperty("--color-text", "#fff");
+    document.documentElement.style.setProperty("--color-text-button", "#fff");
+    document.documentElement.style.setProperty("--color-button", "rgba(255, 255, 255, 0.1");
+    document.documentElement.style.setProperty("--color-button-border", "rgba(255, 255, 255, 0.5");
+    window.sessionStorage.setItem("dark-mode", 1);
+  } else {
+    document.documentElement.style.setProperty("--main-color", "#e9f2f9");
+    document.documentElement.style.setProperty("--color-text", "#000");
+    document.documentElement.style.setProperty("--color-text-button", "#000");
+    document.documentElement.style.setProperty("--color-button", "rgba(0, 0, 0, 0.1");
+    document.documentElement.style.setProperty("--color-button-border", "rgba(0, 0, 0, 0.5");
+    window.sessionStorage.setItem("dark-mode", 0);
+  }
+}
+
+void (function initial_theme() {
+  if (window.sessionStorage.getItem("dark-mode") == 1) {
+    document.getElementById("dark-mode").checked = false; //set mode dark button
+    document.documentElement.style.setProperty("--main-color", "#000");
+    document.documentElement.style.setProperty("--color-text", "#fff");
+    document.documentElement.style.setProperty("--color-text-button", "#fff");
+    document.documentElement.style.setProperty("--color-button", "rgba(255, 255, 255, 0.1");
+    document.documentElement.style.setProperty("--color-button-border", "rgba(255, 255, 255, 0.5");
+  } else {
+    document.getElementById("dark-mode").checked = true; //set mode dark button
+    document.documentElement.style.setProperty("--main-color", "#e9f2f9");
+    document.documentElement.style.setProperty("--color-text", "#000");
+    document.documentElement.style.setProperty("--color-text-button", "#000");
+    document.documentElement.style.setProperty("--color-button", "rgba(0, 0, 0, 0.1");
+    document.documentElement.style.setProperty("--color-button-border", "rgba(0, 0, 0, 0.5");
   }
 })();
